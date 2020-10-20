2cd9aeef5e3f340dafe9494e60f5d4870c8e21d8
SEJUNHONG <57587248+SEJUNHONG@users.noreply.github.com>
2020-10-20 11:04:40 +0900

    Add files via upload

---
+++ Multicampus201020.R
@@ -0,0 +1,16 @@
+insurance<-read.csv("insurance.csv", stringsAsFactors = T)
+str(insurance)
+summary(insurance$expenses)
+hist(insurance$expenses)
+table(insurance$sex)
+table(insurance$smoker)
+table(insurance$region)
+cor(insurance[c("age","bmi","children","expenses")])
+insurance[c("age","bmi","children","expenses")]
+pairs(insurance[c("age","bmi","children","expenses")])
+install.packages("psych")
+library(psych)
+pairs.panels(insurance[c("age","bmi","children","expenses")])
+ins_model<-lm(expenses~age+children+bmi+sex+smoker+region, data = insurance)
+ins_model
+summary(ins_model)
